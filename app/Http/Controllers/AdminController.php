35e6847a689267fd9446cb69aeaed7f983521564
Ashesh <asheshchandro@gmail.com>
2026-03-01 01:30:52 +0600

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -9,6 +11,28 @@ class AdminController extends Controller
     public function index(){
         return view('admin.login');
     }
+
+    public function authenticate(Request $request){
+        $request->validate([
+            'email'=> 'required',
+            'password'=>'required'
+        ]);
+        if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password])){
+            echo "ok";
+        }else{
+            return redirect()->route('admin.loing')->with('error', 'shomething went worng');
+        }
+    }
+
+    public function register(){
+        $user = new User();
+        $user -> name= 'Admin';
+        $user -> role= 'admin';
+        $user -> email= 'admin@gmail.com';
+        $user -> password= 'Hash::make'('admin');
+        return redirect()->route('admin.loing')->with('success', 'shomething went successfully');
+    }
+
 
     public function dashboard(){
         return view('admin.dashboard');
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -27,23 +27,30 @@
             </div>
             <div class="card-body">
                 <p class="login-box-msg">Sign in to start your session</p>
-                <form action="https://adminlte.io/themes/v3/index3.html" method="post">
+                <form action="{{ route('admin.authenticate') }}" method="post">
+                    @csrf
                     <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email">
+                        <input type="email" name="email" class="form-control" placeholder="Email">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
                             </div>
                         </div>
                     </div>
+                    @error('email')
+                    <p class="text-danger">{{ $message }}</p>
+                    @enderror
                     <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Password">
+                        <input type="password" name="password" class="form-control" placeholder="Password">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
                             </div>
                         </div>
                     </div>
+                    @error('password')
+                    <p class="text-danger">{{ $message }}</p>
+                    @enderror
                     <div class="row">
                         <div class="col-8">
                             <div class="icheck-primary">
@@ -73,4 +80,5 @@
 
     <script src="dist/js/adminlte.min2167.js?v=3.2.0"></script>
 </body>
+
 </html>
