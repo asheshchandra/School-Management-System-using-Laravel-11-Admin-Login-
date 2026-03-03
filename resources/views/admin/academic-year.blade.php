fd1b4659445438b75b014d92309c8a46f2121415
Ashesh <asheshchandro@gmail.com>
2026-03-04 02:07:59 +0600

    Updated existing files and added new files for academic year

--- resources/views/admin/academic-year.blade.php
+++ resources/views/admin/academic-year.blade.php
@@ -7,12 +7,12 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1>General Form</h1>
+          <h1>Academic Year</h1>
         </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
-            <li class="breadcrumb-item active">Add Academic Year</li>
+            <li class="breadcrumb-item active">Academic Year</li>
           </ol>
         </div>
       </div>
@@ -25,17 +25,26 @@
 
         <div class="col-md-12">
 
+        @if (Session::has('success'))
+        <div class="alert alert-success">
+          {{ Session::get('success') }}
+        </div>
+        @endif
+
           <div class="card card-primary">
             <div class="card-header">
-              <h3 class="card-title">Quick Example</h3>
+              <h3 class="card-title">Add Academic Year</h3>
             </div>
 
+            @error('name')
+            <p class="text-danger">{{ $message }}</p>
+            @enderror
 
-            <form accept="" method="post">
-              <div class="card-body">
+            <form action="{{ route('academic-year.store') }}" method="post">
+              @csrf <div class="card-body">
                 <div class="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                  <label for="name">Academic Year</label>
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Enter Academic Year" required>
                 </div>
               </div>
 
